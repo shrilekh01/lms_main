681deafa9f937b3a21fd5c72db14a168c601cc7b
shrilekh01 <shrilekhshrikhande06@gmail.com>
2025-04-02 22:58:02 +0530

    shri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './Components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
    <>
-   <ul>
-    <li className='bg-red-400'>Shrilekh</li>
-    <li className='bg-orange-400'>Devanshu</li>
-    <li className='bg-purple-400'>Pranjali</li>
-   </ul>
+   <Button>Let us build something great by shrilekh</Button>
    </>
   )
 }
