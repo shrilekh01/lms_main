c85be4a278dd2d2228b3602e9c759f9a62f5bac0
shrilekh01 <shrilekhshrikhande06@gmail.com>
2025-04-02 10:48:50 +0530

    project setup till client

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <ul>
+    <li className='bg-red-400'>Shrilekh</li>
+    <li className='bg-orange-400'>Devanshu</li>
+   </ul>
+   </>
+  )
+}
+
+export default App
